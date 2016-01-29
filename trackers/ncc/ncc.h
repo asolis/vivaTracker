a6cd44472132c7f3296a4ce8018afc85c9806c2d
Andrés Solís Montero <andres.solis.montero@gmail.com>
2016-01-29 01:53:34 -0500

    edit

--- trackers/ncc/ncc.h
+++ trackers/ncc/ncc.h
@@ -1,3 +1,10 @@
+/*******************************************************
+ * Copyright (C) 2016  Andrés Solís Montero <andres@solism.ca>
+ *   PhD Candidate.
+ *   SITE, University of Ottawa
+ *   800 King Edward Ave.
+ *   Ottawa, On., K1N 6N5, Canada.
+ *******************************************************/
 #ifndef __VivaTracker__ncc__
 #define __VivaTracker__ncc__
 
